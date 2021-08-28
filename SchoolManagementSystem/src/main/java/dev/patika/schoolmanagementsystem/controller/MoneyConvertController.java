192f8269f408d068c0d98667a7414db82a8bc0c9
Tolunay <tasdemirtolunay@gmail.com>
2021-08-29 01:40:55 +0300

    Add MoneyConvertController

--- SchoolManagementSystem/src/main/java/dev/patika/schoolmanagementsystem/controller/MoneyConvertController.java
+++ SchoolManagementSystem/src/main/java/dev/patika/schoolmanagementsystem/controller/MoneyConvertController.java
@@ -1,2 +1,34 @@
-package dev.patika.schoolmanagementsystem.controller;public class MoneyConvertController {
+package dev.patika.schoolmanagementsystem.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/convert")
+public class MoneyConvertController {
+
+    private final static String convertEndPoint = "http://localhost:8080/convert/tltodollar";
+    private final static String convertEndPoint2 = "http://localhost:8080/convert/dollartotl";
+
+    RestTemplate restTemplate = new RestTemplate();
+
+    // Access tltodollar endpoint in ConverOperation module
+    @GetMapping("/Endpoint")
+    public String convertOperation() {
+
+        String result = restTemplate.getForObject(convertEndPoint,String.class);
+        return result;
+    }
+
+    // Access dollartotl endpoint in ConverOperation module
+    @GetMapping("/Endpoint2")
+    public String convertOperation2() {
+
+        String result2 = restTemplate.getForObject(convertEndPoint2,String.class);
+        return result2;
+    }
+
+
 }
